e68bd7846f385158755ea558ad889c3b42a7c910
Jacob Evans <je00059@my.westga.edu>
2025-01-31 15:20:49 -0500

    Updating files

---
+++ code/TicketApp/TicketAppWeb/Data/TestDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace TaskAppWeb.Data
+{
+	public class TestDbContext : DbContext
+	{
+		public TestDbContext(DbContextOptions<TestDbContext> options)
+		: base(options)
+		{ }
+
+		public DbSet<int> Values { get; set; } = null!;
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+	}
+}
