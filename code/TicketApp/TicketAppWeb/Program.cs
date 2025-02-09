487853caf87960e948ef8535da8cf9a88e23e6ef
Jabesi Abwe <ja00069@my.westga.edu>
2025-02-08 21:47:39 -0500

    Added tests for the web app to try and automate the testing and code coverage generation. Added a script tthat runs all the tests projects, run the corage report and build one single report when you run .\run_tests_coverage.ps1 in the developer powershell

--- code/TicketApp/TicketAppWeb/Program.cs
+++ code/TicketApp/TicketAppWeb/Program.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TicketAppWeb
 {
-	public class Program
+    [ExcludeFromCodeCoverage]
+    public class Program
 	{
 		public static void Main(string[] args)
 		{
