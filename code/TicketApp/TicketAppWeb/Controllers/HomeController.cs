f7597ca0b09ccc97daa4004caf91ae09014a18df
Jabesi Abwe <ja00069@my.westga.edu>
2025-02-16 23:39:21 -0500

    Implement a responsive side navigation bar

--- code/TicketApp/TicketAppWeb/Controllers/HomeController.cs
+++ code/TicketApp/TicketAppWeb/Controllers/HomeController.cs
@@ -3,43 +3,52 @@ using TicketAppWeb.Models;
 
 namespace TicketAppWeb.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly TicketAppContext _context;
+    public class HomeController : Controller
+    {
+        private readonly TicketAppContext _context;
 
-		public HomeController(TicketAppContext context)
-		{
-			_context = context;
-		}
+        public HomeController(TicketAppContext context)
+        {
+            _context = context;
+        }
 
-		[HttpGet]
-		public IActionResult Index()
-		{
-			var numbersList = _context.Numbers.OrderByDescending(n => n.Id).ToList();
-			ViewBag.FV = numbersList.FirstOrDefault()?.Value ?? 0;
-			
-			return View(numbersList);
-		}
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var numbersList = _context.Numbers.OrderByDescending(n => n.Id).ToList();
+            ViewBag.FV = numbersList.FirstOrDefault()?.Value ?? 0;
 
-		[HttpPost]
-		public IActionResult Index(double value)
-		{
-			if (value < 0)
-			{
-				ViewBag.FV = 0;
-				ModelState.AddModelError("Value", "Please enter a valid number.");
-			}
-			else
-			{
-				var sentNumber = new Number(value);
-				_context.Numbers.Add(sentNumber);
-				_context.SaveChanges();
-			}
+            return RedirectToAction("UserManagement");
+        }
 
-			var numbersList = _context.Numbers.OrderByDescending(n => n.Id).ToList();
-			ViewBag.FV = value;
+        [HttpPost]
+        public IActionResult Index(double value)
+        {
+            if (value < 0)
+            {
+                ViewBag.FV = 0;
+                ModelState.AddModelError("Value", "Please enter a valid number.");
+            }
+            else
+            {
+                var sentNumber = new Number(value);
+                _context.Numbers.Add(sentNumber);
+                _context.SaveChanges();
+            }
 
-			return View(numbersList);
-		}
-	}
+            var numbersList = _context.Numbers.OrderByDescending(n => n.Id).ToList();
+            ViewBag.FV = value;
+
+            return RedirectToAction("UserManagement");
+        }
+
+        [HttpGet]
+        public IActionResult UserManagement()
+        {
+            var numbersList = _context.Numbers.OrderByDescending(n => n.Id).ToList();
+            ViewBag.FV = numbersList.FirstOrDefault()?.Value ?? 0;
+
+            return View(numbersList);
+        }
+    }
 }
