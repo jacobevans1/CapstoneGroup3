6b69ffb4af758504b779d08344a66746bd4f4a50
Jacob Evans <je00059@my.westga.edu>
2025-02-18 21:28:51 -0500

    Seeded Admin User
    1) Created an Master Admin user for the system
    
    2) Fixed bad migration files

---
+++ code/TicketApp/TicketAppWeb/Migrations/20250219022546_SeedAdminFixed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketAppWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedAdminFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1b1c2d3e-4f5g-678h-91ij-23456789ab5d", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "b74ddd14-6340-4840-95c2-db12554843e5", 0, "93ba2f22-16e5-4753-99cc-6cab07467e79", "admin@example.com", true, "System", "Administrator", false, null, "ADMIN@EXAMPLE.COM", "ADMIN", "AQAAAAIAAYagAAAAECxosoODgz+v/No4ynkryfzVjdi9+zi6EzYlKJ91Vgn1SuRJYnpgNdZAcgZnXcEQMw==", null, false, "A1B2C3D4E5F6G7H8I9J0", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1b1c2d3e-4f5g-678h-91ij-23456789ab5d", "b74ddd14-6340-4840-95c2-db12554843e5" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1b1c2d3e-4f5g-678h-91ij-23456789ab5d", "b74ddd14-6340-4840-95c2-db12554843e5" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1b1c2d3e-4f5g-678h-91ij-23456789ab5d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5");
+        }
+    }
+}
--- code/TicketApp/TicketAppWeb/Models/DataLayer/TicketAppContext.cs
+++ code/TicketApp/TicketAppWeb/Models/DataLayer/TicketAppContext.cs
@@ -1,16 +1,54 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TicketAppWeb.Models.DomainModels;
 
 namespace TicketAppWeb.Models.DataLayer
 {
-    public class TicketAppContext : IdentityDbContext<TicketAppUser>
-    {
-        public TicketAppContext(DbContextOptions<TicketAppContext> options) : base(options) { }
+	public class TicketAppContext : IdentityDbContext<TicketAppUser>
+	{
+		public TicketAppContext(DbContextOptions<TicketAppContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Admin user
+			var adminUser = new TicketAppUser
+			{
+				Id = "b74ddd14-6340-4840-95c2-db12554843e5",
+				UserName = "admin",
+				NormalizedUserName = "ADMIN",
+				Email = "admin@example.com",
+				NormalizedEmail = "ADMIN@EXAMPLE.COM",
+				EmailConfirmed = true,
+				FirstName = "System",
+				LastName = "Administrator",
+				SecurityStamp = "A1B2C3D4E5F6G7H8I9J0",
+				PasswordHash = "AQAAAAIAAYagAAAAECxosoODgz+v/No4ynkryfzVjdi9+zi6EzYlKJ91Vgn1SuRJYnpgNdZAcgZnXcEQMw==",
+				ConcurrencyStamp = "93ba2f22-16e5-4753-99cc-6cab07467e79",
+			};
+
+			modelBuilder.Entity<TicketAppUser>().HasData(adminUser);
+
+
+			// Admin role
+			var adminRole = new IdentityRole
+			{
+				Id = "1b1c2d3e-4f5g-678h-91ij-23456789ab5d",
+				Name = "Admin",
+				NormalizedName = "ADMIN",
+			};
+
+			modelBuilder.Entity<IdentityRole>().HasData(adminRole);
+
+
+			// Admin user role
+			modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+			{
+				UserId = adminUser.Id,
+				RoleId = adminRole.Id,
+			});
+		}
+	}
 }
