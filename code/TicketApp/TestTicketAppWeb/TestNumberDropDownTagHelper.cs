e88264b158c5fd2a13ed482a4664a2a05fde3d86
Jabesi Abwe <ja00069@my.westga.edu>
2025-02-23 13:56:08 -0500

    Tested number drop dow tag helper

---
+++ code/TicketApp/TestTicketAppWeb/TestNumberDropDownTagHelper.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketAppWeb.TagHelpers;
+
+namespace TestTicketAppWeb;
+
+public class TestNumberDropDownTagHelper
+{
+    [Fact]
+    public void Process_GeneratesCorrectOptions()
+    {
+        // Arrange
+        var tagHelper = new NumberDropDownTagHelper
+        {
+            Min = 1,
+            Max = 5
+        };
+
+        var tagHelperContext = new TagHelperContext(
+            new TagHelperAttributeList
+            {
+                    { "asp-for", new ModelExpression("Number", CreateModelExplorer(3)) }
+            },
+            new Dictionary<object, object>(),
+            "test");
+
+        var tagHelperOutput = new TagHelperOutput(
+            "select",
+            new TagHelperAttributeList(),
+            (useCachedResult, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetContent("");
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            });
+
+        // Act
+        tagHelper.Process(tagHelperContext, tagHelperOutput);
+
+        // Assert
+        var outputContent = tagHelperOutput.Content.GetContent();
+        for (int i = 1; i <= 5; i++)
+        {
+            Assert.Contains($"<option{(i == 3 ? " selected=\"selected\"" : "")}>{i}</option>", outputContent);
+        }
+    }
+
+    [Fact]
+    public void Process_MinEqualsMax_GeneratesSingleOption()
+    {
+        // Arrange
+        var tagHelper = new NumberDropDownTagHelper
+        {
+            Min = 5,
+            Max = 5
+        };
+
+        var tagHelperContext = new TagHelperContext(
+            new TagHelperAttributeList
+            {
+                    { "asp-for", new ModelExpression("Number", CreateModelExplorer(5)) }
+            },
+            new Dictionary<object, object>(),
+            "test");
+
+        var tagHelperOutput = new TagHelperOutput(
+            "select",
+            new TagHelperAttributeList(),
+            (useCachedResult, encoder) =>
+            {
+                var tagHelperContent = new DefaultTagHelperContent();
+                tagHelperContent.SetContent("");
+                return Task.FromResult<TagHelperContent>(tagHelperContent);
+            });
+
+        // Act
+        tagHelper.Process(tagHelperContext, tagHelperOutput);
+
+        // Assert
+        var outputContent = tagHelperOutput.Content.GetContent();
+        Assert.Contains("<option selected=\"selected\">5</option>", outputContent);
+    }
+
+    private static ModelExplorer CreateModelExplorer(object model)
+    {
+        var metadataProvider = new EmptyModelMetadataProvider();
+        var metadata = metadataProvider.GetMetadataForType(model.GetType());
+        var modelExplorer = new ModelExplorer(metadataProvider, metadata, model);
+        return modelExplorer;
+    }
+}
