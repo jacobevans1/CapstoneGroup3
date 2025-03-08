86fedcc8f840e3e43c8ff296fad58211330d7254
Jacob Evans <je00059@my.westga.edu>
2025-03-07 22:22:09 -0500

    Fixes Issue with user login
    1) Fixes issue with login where a created user was unable to login
    
    2) Updated the seeded accounts to use same username and password format as rest of system

--- code/TicketApp/TicketAppWeb/Controllers/LoginController.cs
+++ code/TicketApp/TicketAppWeb/Controllers/LoginController.cs
@@ -1,63 +1,62 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 using TicketAppWeb.Models.DomainModels;
 
 namespace TicketAppWeb.Controllers
 {
-    [AllowAnonymous] // Allows anyone to access login without being signed in
+	[AllowAnonymous] // Allows anyone to access login without being signed in
 
-    public class LoginController : Controller
-    {
-        private readonly SignInManager<TicketAppUser> _signInManager;
-        private readonly UserManager<TicketAppUser> _userManager;
+	public class LoginController : Controller
+	{
+		private readonly SignInManager<TicketAppUser> _signInManager;
+		private readonly UserManager<TicketAppUser> _userManager;
 
-        public LoginController(SignInManager<TicketAppUser> signInManager, UserManager<TicketAppUser> userManager)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
+		public LoginController(SignInManager<TicketAppUser> signInManager, UserManager<TicketAppUser> userManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Index(string username, string password)
-        {
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            {
-                ViewBag.Error = "Username and password are required.";
-                return View();
-            }
+		[HttpPost]
+		public async Task<IActionResult> Index(string username, string password)
+		{
+			if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+			{
+				ViewBag.Error = "Username and password are required.";
+				return View();
+			}
 
-            var user = await _userManager.FindByNameAsync(username);
-            if (user == null)
-            {
-                ViewBag.Error = "Invalid username or password.";
-                return View();
-            }
+			var user = await _userManager.FindByNameAsync(username);
+			if (user == null)
+			{
+				ViewBag.Error = "Invalid username or password.";
+				return View();
+			}
 
-            var result = await _signInManager.PasswordSignInAsync(user, password, isPersistent: false, lockoutOnFailure: false);
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ViewBag.Error = "Invalid username or password.";
-                return View();
-            }
-        }
+			var result = await _signInManager.PasswordSignInAsync(user, password, isPersistent: false, lockoutOnFailure: false);
+			if (result.Succeeded)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ViewBag.Error = "Invalid username or password.";
+				return View();
+			}
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Login");
-        }
-    }
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index", "Login");
+		}
+	}
 }
--- code/TicketApp/TicketAppWeb/Models/Configuration/SeedData.cs
+++ code/TicketApp/TicketAppWeb/Models/Configuration/SeedData.cs
@@ -10,77 +10,77 @@ namespace TicketAppWeb.Models.Configuration
 	/// </summary>
 	public class SeedData
 	{
-        public static async Task Initialize(IServiceProvider serviceProvider, UserManager<TicketAppUser> userManager, RoleManager<IdentityRole> roleManager)
-        {
-            var roleNames = new[] { "Admin", "User" };
+		public static async Task Initialize(IServiceProvider serviceProvider, UserManager<TicketAppUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			var roleNames = new[] { "Admin", "User" };
 
-            foreach (var roleName in roleNames)
-            {
-                var roleExist = await roleManager.RoleExistsAsync(roleName);
-                if (!roleExist)
-                {
-                    await roleManager.CreateAsync(new IdentityRole(roleName));
-                }
-            }
+			foreach (var roleName in roleNames)
+			{
+				var roleExist = await roleManager.RoleExistsAsync(roleName);
+				if (!roleExist)
+				{
+					await roleManager.CreateAsync(new IdentityRole(roleName));
+				}
+			}
 
-            // Create Admin User
-            var adminUser = await userManager.FindByEmailAsync("admin@domain.com");
-            if (adminUser == null)
-            {
-                adminUser = new TicketAppUser
-                {
-                    UserName = "Admin",
-                    FirstName = "Admin",
-                    LastName = "User",
-                    Email = "admin@domain.com",
-                    EmailConfirmed = true
-                };
+			// Create Admin User
+			var adminUser = await userManager.FindByEmailAsync("admin@domain.com");
+			if (adminUser == null)
+			{
+				adminUser = new TicketAppUser
+				{
+					UserName = "Admin",
+					FirstName = "Admin",
+					LastName = "User",
+					Email = "admin@domain.com",
+					EmailConfirmed = true
+				};
 
-                var result = await userManager.CreateAsync(adminUser, "Admin123!");
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(adminUser, "Admin");
-                }
-            }
+				var result = await userManager.CreateAsync(adminUser, "Admin123!");
+				if (result.Succeeded)
+				{
+					await userManager.AddToRoleAsync(adminUser, "Admin");
+				}
+			}
 
-            // Create standard users
-            await CreateUserIfNotExists(userManager, "jabesi@domain.com", "Jabesi", "Abwe", "User");
-            await CreateUserIfNotExists(userManager, "jacob@domain.com", "Jacob", "Evans", "User");
-        }
+			// Create standard users
+			await CreateUserIfNotExists(userManager, "jabesi@domain.com", "Jabesi", "Abwe", "User");
+			await CreateUserIfNotExists(userManager, "jacob@domain.com", "Jacob", "Evans", "User");
+		}
 
-        /// <summary>
-        /// Creates a user if they do not already exist.
-        /// </summary>
-        private static async Task CreateUserIfNotExists(UserManager<TicketAppUser> userManager, string email, string firstName, string lastName, string role)
-        {
-            var user = await userManager.FindByEmailAsync(email);
-            if (user == null)
-            {
-                user = new TicketAppUser
-                {
-                    UserName = firstName,  // Using first name as the username
-                    FirstName = firstName,
-                    LastName = lastName,
-                    Email = email,
-                    EmailConfirmed = true
-                };
+		/// <summary>
+		/// Creates a user if they do not already exist.
+		/// </summary>
+		private static async Task CreateUserIfNotExists(UserManager<TicketAppUser> userManager, string email, string firstName, string lastName, string role)
+		{
+			var user = await userManager.FindByEmailAsync(email);
+			if (user == null)
+			{
+				user = new TicketAppUser
+				{
+					UserName = firstName + lastName,
+					FirstName = firstName,
+					LastName = lastName,
+					Email = email,
+					EmailConfirmed = true
+				};
 
-                var result = await userManager.CreateAsync(user, firstName + "123!");
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(user, role);
-                }
-            }
-        }
+				var result = await userManager.CreateAsync(user, firstName + lastName + "123!");
+				if (result.Succeeded)
+				{
+					await userManager.AddToRoleAsync(user, role);
+				}
+			}
+		}
 
-        /// <summary>
-        ///	Adds users to the database.
-        /// </summary>
-        /// <param name="serviceProvider"></param>
-        /// <param name="userManager"></param>
-        /// <param name="roleManager"></param>
-        /// <returns></returns>
-        public static async Task AddUsers(IServiceProvider serviceProvider, UserManager<TicketAppUser> userManager, RoleManager<IdentityRole> roleManager)
+		/// <summary>
+		///	Adds users to the database.
+		/// </summary>
+		/// <param name="serviceProvider"></param>
+		/// <param name="userManager"></param>
+		/// <param name="roleManager"></param>
+		/// <returns></returns>
+		public static async Task AddUsers(IServiceProvider serviceProvider, UserManager<TicketAppUser> userManager, RoleManager<IdentityRole> roleManager)
 		{
 		}
 	}
