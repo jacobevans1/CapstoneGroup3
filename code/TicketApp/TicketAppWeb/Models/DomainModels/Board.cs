7a7f5afa7535ef7a2d5662b1a6abddf138b90245
Jacob Evans <je00059@my.westga.edu>
2025-03-15 16:11:57 -0400

    Added the Board class
    1) Added the Board class

---
+++ code/TicketApp/TicketAppWeb/Models/DomainModels/Board.cs
@@ -0,0 +1,32 @@
+﻿// Capstone Group 3
+// Spring 2025
+namespace TicketAppWeb.Models.DomainModels
+{
+	/// <summary>
+	/// Represents a project board that tracks stages of tickets
+	/// </summary>
+	public class Board
+	{
+		/// <summary>
+		/// Gets or sets the board identifier.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the board.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description of the board.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the project identifier.
+		/// </summary>
+		public string ProjectId { get; set; }
+
+		//public List<Ticket> Tickets { get; set; }
+	}
+}
--- code/TicketApp/TicketAppWeb/Models/DomainModels/Group.cs
+++ code/TicketApp/TicketAppWeb/Models/DomainModels/Group.cs
@@ -2,48 +2,50 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+// Capstone Group 3
+// Spring 2025
 namespace TicketAppWeb.Models.DomainModels;
 
 public class Group
 {
-    // Constructor
-    public Group()
-    {
-        Id = Guid.NewGuid().ToString(); // Auto-generate ID if not provided
-        Members = new HashSet<TicketAppUser>();
-        Projects = new HashSet<Project>();
-        CreatedAt = DateTime.UtcNow; // Set creation time on object initialization
-    }
+	// Constructor
+	public Group()
+	{
+		Id = Guid.NewGuid().ToString(); // Auto-generate ID if not provided
+		Members = new HashSet<TicketAppUser>();
+		Projects = new HashSet<Project>();
+		CreatedAt = DateTime.UtcNow; // Set creation time on object initialization
+	}
 
-    // Group Id
-    [Key]
-    public string Id { get; set; }
+	// Group Id
+	[Key]
+	public string Id { get; set; }
 
-    // Group name
-    [Required(ErrorMessage = "Please enter a group name")]
-    [StringLength(100, ErrorMessage = "Group name cannot exceed 100 characters")]
-    public string GroupName { get; set; } = string.Empty;
+	// Group name
+	[Required(ErrorMessage = "Please enter a group name")]
+	[StringLength(100, ErrorMessage = "Group name cannot exceed 100 characters")]
+	public string GroupName { get; set; } = string.Empty;
 
-    // Group description
-    [Required(ErrorMessage = "Please enter a group description")]
-    [StringLength(255, ErrorMessage = "Description cannot exceed 255 characters")]
-    public string Description { get; set; } = string.Empty;
+	// Group description
+	[Required(ErrorMessage = "Please enter a group description")]
+	[StringLength(255, ErrorMessage = "Description cannot exceed 255 characters")]
+	public string Description { get; set; } = string.Empty;
 
-    // Group manager ID
-    [Required(ErrorMessage = "Please select a group manager")]
-    public string ManagerId { get; set; }
+	// Group manager ID
+	[Required(ErrorMessage = "Please select a group manager")]
+	public string ManagerId { get; set; }
 
-    // Navigation property for the manager (linked to `AspNetUsers`)
-    [ForeignKey("ManagerId")]
-    [ValidateNever]
-    public virtual TicketAppUser? Manager { get; set; }
+	// Navigation property for the manager (linked to `AspNetUsers`)
+	[ForeignKey("ManagerId")]
+	[ValidateNever]
+	public virtual TicketAppUser? Manager { get; set; }
 
-    // Date when the group was created
-    public DateTime CreatedAt { get; private set; }
+	// Date when the group was created
+	public DateTime CreatedAt { get; private set; }
 
-    // Members of the group
-    public virtual ICollection<TicketAppUser> Members { get; set; }
+	// Members of the group
+	public virtual ICollection<TicketAppUser> Members { get; set; }
 
-    // Projects linked to the group
-    public virtual ICollection<Project> Projects { get; set; }
+	// Projects linked to the group
+	public virtual ICollection<Project> Projects { get; set; }
 }
