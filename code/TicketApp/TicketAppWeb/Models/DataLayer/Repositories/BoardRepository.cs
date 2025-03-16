ca822dafc87d91774671eb1185f47af182044bd9
Jacob Evans <je00059@my.westga.edu>
2025-03-16 14:46:06 -0400

    Created the basic BoardRepository interface
    1) Created the basic BoardRepository interface and it's implementation

---
+++ code/TicketApp/TicketAppWeb/Models/DataLayer/Repositories/BoardRepository.cs
@@ -0,0 +1,23 @@
+﻿// Capstone Group 3
+// Spring 2025
+using TicketAppWeb.Models.DataLayer.Repositories.Interfaces;
+using TicketAppWeb.Models.DomainModels;
+
+namespace TicketAppWeb.Models.DataLayer.Repositories
+{
+	/// <summary>
+	/// The BoardRepository class implements IBoardRepository
+	/// </summary>
+	public class BoardRepository : Repository<Board>, IBoardRepository
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BoardRepository"/> class.
+		/// </summary>
+		/// <param name="ctx"></param>
+		public BoardRepository(TicketAppContext ctx) : base(ctx)
+		{
+		}
+
+
+	}
+}
---
+++ code/TicketApp/TicketAppWeb/Models/DataLayer/Repositories/Interfaces/IBoardRepository.cs
@@ -0,0 +1,14 @@
+﻿using TicketAppWeb.Models.DomainModels;
+
+// Capstone Group 3
+// Spring 2025
+namespace TicketAppWeb.Models.DataLayer.Repositories.Interfaces
+{
+	/// <summary>
+	/// The IBoardRepository interface defines the methods that must be implemented by all Board repository classes.
+	/// </summary>
+	public interface IBoardRepository : IRepository<Board>
+	{
+
+	}
+}
