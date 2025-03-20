3d1debea534edc7991754cb093768106633a8b17
Jabesi Abwe <ja00069@my.westga.edu>
2025-03-20 17:16:01 -0400

    Tested extention Methods and tag helper for 100% branch coverage
    1) Tested missing branches in TempMeaasgeTagHelper
    2) Edited my script to generate the code coverage to exclude Program.cs

--- code/TicketApp/TestTicketAppWeb/TagHelpers/TestTempMessageTagHelper.cs
+++ code/TicketApp/TestTicketAppWeb/TagHelpers/TestTempMessageTagHelper.cs
@@ -4,113 +4,144 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Moq;
 using TicketAppWeb.TagHelpers;
+using Xunit;
 
-namespace TestTicketAppWeb.TagHelpers;
-
-/// <summary>
-/// Tests the TempMessageTagHelper
-/// Jabesi Abwe 
-/// 02/23/2024
-/// </summary>
-public class TestTempMessageTagHelper
+namespace TestTicketAppWeb.TagHelpers
 {
-	private static TempDataDictionary CreateTempData(Dictionary<string, object?> values)
-	{
-		var httpContext = new DefaultHttpContext();
-		var tempDataProviderMock = new Mock<ITempDataProvider>();
-		var tempData = new TempDataDictionary(httpContext, tempDataProviderMock.Object);
+    /// <summary>
+    /// Tests the TempMessageTagHelper
+    /// Jabesi Abwe 
+    /// 02/23/2024
+    /// </summary>
+    public class TestTempMessageTagHelper
+    {
+        private static TempDataDictionary CreateTempData(Dictionary<string, object?> values)
+        {
+            var httpContext = new DefaultHttpContext();
+            var tempDataProviderMock = new Mock<ITempDataProvider>();
+            var tempData = new TempDataDictionary(httpContext, tempDataProviderMock.Object);
 
-		foreach (var kvp in values)
-		{
-			tempData[kvp.Key] = kvp.Value;
-		}
+            foreach (var kvp in values)
+            {
+                tempData[kvp.Key] = kvp.Value;
+            }
 
-		return tempData;
-	}
+            return tempData;
+        }
 
-	[Theory]
-	[InlineData("SuccessMessage", "Success!", "bg-success bg-opacity-25 text-center text-success p-2")]
-	[InlineData("SuccessMessage", "", "bg-success bg-opacity-25 text-center text-success p-2")]
-	[InlineData("ErrorMessage", "Error occurred", "bg-danger bg-opacity-25 text-center text-danger p-2")]
-	[InlineData("ErrorMessage", "", "bg-danger bg-opacity-25 text-center text-danger p-2")]
-	[InlineData("ErrorMessage", null, "bg-danger bg-opacity-25 text-center text-danger p-2")]
-	[InlineData("", null, "")]
-	public void Process_ShouldSetCorrectMessageAndClasses(string key, string? message, string expectedClass)
-	{
-		// Arrange
-		var tagHelper = new TempMessageTagHelper
-		{
-			ViewCtx = new ViewContext { TempData = CreateTempData(new Dictionary<string, object?> { { key, message } }) }
-		};
+        [Theory]
+        [InlineData("SuccessMessage", "Success!", "bg-success bg-opacity-25 text-center text-success p-2")]
+        [InlineData("SuccessMessage", "", "bg-success bg-opacity-25 text-center text-success p-2")]
+        [InlineData("ErrorMessage", "Error occurred", "bg-danger bg-opacity-25 text-center text-danger p-2")]
+        [InlineData("ErrorMessage", "", "bg-danger bg-opacity-25 text-center text-danger p-2")]
+        [InlineData("ErrorMessage", null, "bg-danger bg-opacity-25 text-center text-danger p-2")]
+        [InlineData("", null, "")]
+        public void Process_ShouldSetCorrectMessageAndClasses(string key, string? message, string expectedClass)
+        {
+            // Arrange
+            var tagHelper = new TempMessageTagHelper
+            {
+                ViewCtx = new ViewContext { TempData = CreateTempData(new Dictionary<string, object?> { { key, message } }) }
+            };
 
-		var context = new TagHelperContext(
-			new TagHelperAttributeList(),
-			new Dictionary<object, object>(),
-			"test"
-		);
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "test"
+            );
 
-		var output = new TagHelperOutput(
-			"my-temp-message",
-			new TagHelperAttributeList(),
-			(useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
-		);
+            var output = new TagHelperOutput(
+                "my-temp-message",
+                new TagHelperAttributeList(),
+                (useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
+            );
 
-		// Act
-		tagHelper.Process(context, output);
+            // Act
+            tagHelper.Process(context, output);
 
-		// Assert
-		if (key == "message" && (message == null || message == ""))
-		{
-			Assert.Equal("h4", output.TagName);
-			Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
-			Assert.Equal(string.Empty, output.Content.GetContent());
-		}
-		else if (key == "ErrorMessage" && (message == null || message == ""))
-		{
-			Assert.Equal("h4", output.TagName);
-			Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
-			Assert.Equal(string.Empty, output.Content.GetContent());
-		}
-		else if (string.IsNullOrEmpty(key) && message == null)
-		{
-			Assert.Null(output.TagName);
-			Assert.Empty(output.Content.GetContent());
-		}
-		else
-		{
-			Assert.Equal("h4", output.TagName);
-			Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
-			Assert.Equal(message, output.Content.GetContent());
-		}
-	}
+            // Assert
+            if (key == "SuccessMessage" && (message == null || message == ""))
+            {
+                Assert.Equal("h4", output.TagName);
+                Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
+                Assert.Equal(string.Empty, output.Content.GetContent());
+            }
+            else if (key == "ErrorMessage" && (message == null || message == ""))
+            {
+                Assert.Equal("h4", output.TagName);
+                Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
+                Assert.Equal(string.Empty, output.Content.GetContent());
+            }
+            else if (string.IsNullOrEmpty(key) && message == null)
+            {
+                Assert.Null(output.TagName);
+                Assert.Empty(output.Content.GetContent());
+            }
+            else
+            {
+                Assert.Equal("h4", output.TagName);
+                Assert.Contains(expectedClass, output.Attributes["class"]?.Value.ToString());
+                Assert.Equal(message, output.Content.GetContent());
+            }
+        }
 
-	[Fact]
-	public void Process_ShouldSuppressOutput_WhenNoMessage()
-	{
-		// Arrange
-		var tagHelper = new TempMessageTagHelper
-		{
-			ViewCtx = new ViewContext { TempData = CreateTempData(new Dictionary<string, object?>()) }
-		};
+        [Fact]
+        public void Process_ShouldSuppressOutput_WhenNoMessage()
+        {
+            // Arrange
+            var tagHelper = new TempMessageTagHelper
+            {
+                ViewCtx = new ViewContext { TempData = CreateTempData(new Dictionary<string, object?>()) }
+            };
 
-		var context = new TagHelperContext(
-			new TagHelperAttributeList(),
-			new Dictionary<object, object>(),
-			"test"
-		);
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "test"
+            );
 
-		var output = new TagHelperOutput(
-			"my-temp-message",
-			new TagHelperAttributeList(),
-			(useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
-		);
+            var output = new TagHelperOutput(
+                "my-temp-message",
+                new TagHelperAttributeList(),
+                (useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
+            );
 
-		// Act
-		tagHelper.Process(context, output);
+            // Act
+            tagHelper.Process(context, output);
 
-		// Assert
-		Assert.Null(output.TagName);
-		Assert.Empty(output.Content.GetContent());
-	}
+            // Assert
+            Assert.Null(output.TagName);
+            Assert.Empty(output.Content.GetContent());
+        }
+
+        [Fact]
+        public void Process_ShouldHandleNullSuccessMessage()
+        {
+            // Arrange
+            var tagHelper = new TempMessageTagHelper
+            {
+                ViewCtx = new ViewContext { TempData = CreateTempData(new Dictionary<string, object?> { { "SuccessMessage", null } }) }
+            };
+
+            var context = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                "test"
+            );
+
+            var output = new TagHelperOutput(
+                "my-temp-message",
+                new TagHelperAttributeList(),
+                (useCachedResult, encoder) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent())
+            );
+
+            // Act
+            tagHelper.Process(context, output);
+
+            // Assert
+            Assert.Equal("h4", output.TagName);
+            Assert.Contains("bg-success bg-opacity-25 text-center text-success p-2", output.Attributes["class"]?.Value.ToString());
+            Assert.Equal(string.Empty, output.Content.GetContent());
+        }
+    }
 }
-
