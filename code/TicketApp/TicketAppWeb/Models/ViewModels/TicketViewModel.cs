4fba9168c49d34923ed1e1baceebba302eb98587
Jabesi Abwe <ja00069@my.westga.edu>
2025-04-12 15:57:40 -0400

    Implement Edit Ticket Info feature
    1) It works prety much like add

--- code/TicketApp/TicketAppWeb/Models/ViewModels/TicketViewModel.cs
+++ code/TicketApp/TicketAppWeb/Models/ViewModels/TicketViewModel.cs
@@ -3,63 +3,64 @@ using TicketAppWeb.Models.DomainModels;
 
 // Capstone Group 3
 // Spring 2025
-namespace TicketAppWeb.Models.ViewModels
+namespace TicketAppWeb.Models.ViewModels;
+
+public class TicketViewModel
 {
-	public class TicketViewModel
+	/// <summary>
+	/// The currently logged in user
+	/// </summary>
+	[ValidateNever]
+	public TicketAppUser CurrentUser { get; set; } = new();
+
+	/// <summary>
+	/// The currently logged in user's role.
+	/// </summary>
+	[ValidateNever]
+	public string? CurrentUserRole { get; set; }
+
+	/// <summary>
+	/// Gets or sets the current ticket.
+	/// </summary>
+	[ValidateNever]
+	public Ticket Ticket { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the new ticket to add.
+	/// </summary>
+	[ValidateNever]
+	public Ticket NewTicket { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the project associated with the ticket.
+	/// </summary>
+	[ValidateNever]
+	public Project Project { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the board associated with the ticket.
+	/// </summary>
+	[ValidateNever]
+	public Board Board { get; set; } = new();
+
+	/// <summary>
+	/// Gets or sets the selected user ID.
+	/// </summary>
+	public string SelectedUserId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Gets or sets the selected stage ID.
+	/// </summary>
+	public string SelectedStageId { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Initializes a new instance of the TicketViewModel class.
+	/// </summary>
+	public TicketViewModel()
 	{
-		/// <summary>
-		/// The currently logged in user
-		/// </summary>
-		[ValidateNever]
-		public TicketAppUser CurrentUser { get; set; }
-
-		/// <summary>
-		/// The currently logged in user's role.
-		/// </summary>
-		[ValidateNever]
-		public string? CurrentUserRole { get; set; }
-
-		/// <summary>
-		/// Gets or sets the current ticket.
-		/// </summary>
-		[ValidateNever]
-		public Ticket Ticket { get; set; }
-
-		/// <summary>
-		/// Gets or sets the new ticket to add.
-		/// </summary>
-		[ValidateNever]
-		public Ticket NewTicket { get; set; }
-
-		/// <summary>
-		/// Gets or sets the project associated with the ticket.
-		/// </summary>
-		public Project Project { get; set; }
-
-		/// <summary>
-		/// Gets or sets the board associated with the ticket.
-		/// </summary>
-		public Board Board { get; set; }
-
-		/// <summary>
-		/// Gets or sets the selected user ID.
-		/// </summary>
-		public string SelectedUserId { get; set; }
-
-		/// <summary>
-		/// Gets or sets the selected stage ID.
-		/// </summary>
-		public string SelectedStageId { get; set; }
-
-		/// <summary>
-		/// Initializes a new instance of the TicketViewModel class.
-		/// </summary>
-		public TicketViewModel()
-		{
-			Ticket = new Ticket();
-			NewTicket = new Ticket();
-			Project = new Project();
-			Board = new Board();
-		}
+		Ticket = new Ticket();
+		NewTicket = new Ticket();
+		Project = new Project();
+		Board = new Board();
 	}
 }
